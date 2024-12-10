7ea1ffd2ea4ddd4701d071a35be24476a70fdfcd
Hüseyin <hkoc274@gmail.com>
2024-12-11 01:43:57 +0300

    AdminPage Düzenlemeleri

---
+++ Portfolio_Proje/Controllers/SkillController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntitiyFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Proje.Controllers
+{
+    public class SkillController : Controller
+    {
+        SkillManager skillManager = new SkillManager(new EfSkill());
+        public IActionResult Index()
+        {
+            var values = skillManager.TGetList();
+            return View(values);
+        }
+    }
+}
