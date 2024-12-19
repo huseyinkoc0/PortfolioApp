b522f1b6720c34d0d3629c23522b891a653395c7
Hüseyin <hkoc274@gmail.com>
2024-12-20 02:51:14 +0300

    commit

---
+++ Portfolio_Proje/Controllers/PortfolioController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntitiyFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Proje.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolio());
+        public IActionResult Index()
+        {
+            var values=portfolioManager.TGetList();
+            return View(values);
+        }
+    }
+}
