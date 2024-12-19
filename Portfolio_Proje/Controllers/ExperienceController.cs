9b0bb7cc0531f262450c96cdf01c468e1e144d02
Hüseyin <hkoc274@gmail.com>
2024-12-20 01:18:46 +0300

    Admin Experience işlemleri yapıldı.

---
+++ Portfolio_Proje/Controllers/ExperienceController.cs
@@ -0,0 +1,60 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntitiyFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Proje.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperience());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Deneyim Listesi";
+            ViewBag.v2 = "Deneyimler";
+            ViewBag.v3 = "Deneyim Listesi";
+            var values = experienceManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddExperience() {
+
+            return View();      
+        
+        }
+        [HttpPost]
+        public IActionResult AddExperience(Experience experience)
+        {
+
+            experienceManager.TAdd(experience);
+
+            return RedirectToAction("Index");
+
+
+        }
+         public IActionResult DeleteExperience(int id)
+        {
+           var values=experienceManager.TGetById(id);
+            experienceManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditExperience(int id)
+        {
+            ViewBag.v1 = "Düzenleme";
+            ViewBag.v2 = "Deneyimler";
+            ViewBag.v3 = "Deneyim Düzenleme";
+            var values= experienceManager.TGetById(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditExperience(Experience experience)
+        {
+           
+            experienceManager.TUpdate(experience);
+            return RedirectToAction("Index");
+        }
+    }
+}
