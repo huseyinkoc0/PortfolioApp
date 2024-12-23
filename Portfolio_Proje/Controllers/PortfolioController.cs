6dce2a3e8e49f32524ae3da626fe02c80973e44e
Hüseyin <hkoc274@gmail.com>
2024-12-24 02:34:58 +0300

    Ekleme ve silme işlemleri yapıldı

--- Portfolio_Proje/Controllers/PortfolioController.cs
+++ Portfolio_Proje/Controllers/PortfolioController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntitiyFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Portfolio_Proje.Controllers
@@ -27,10 +29,66 @@ namespace Portfolio_Proje.Controllers
         [HttpPost]
         public IActionResult AddPortfolio(Portfolio portfolio) 
         {
+            PortfolioValidator validations = new PortfolioValidator();
+            ValidationResult results= validations.Validate(portfolio);
+            if (results.IsValid)
 
-            portfolioManager.TAdd(portfolio);
+            {
 
+                portfolioManager.TAdd(portfolio);
+
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                foreach(var item in results.Errors) 
+                {
+                    ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                }
+            }
+
+            return View();
+
+        }
+
+        public IActionResult DeletePortfolio(int id)
+        {
+            var values=portfolioManager.TGetById(id);
+            portfolioManager.TDelete(values);
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        public IActionResult EditPortfolio(int id)
+        {
+            ViewBag.v1 = "Proje listesi";
+            ViewBag.v2 = "Projeler ";
+            ViewBag.v3 = "Proje Güncelleme";
+            var values = portfolioManager.TGetById(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditPortfolio(Portfolio portfolio)
+        {
+            PortfolioValidator validator = new PortfolioValidator();
+            var result=validator.Validate(portfolio);
+            if (result.IsValid)
+            {
+                portfolioManager.TUpdate(portfolio);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach(var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                }
+
+            }
+            return View();
+
+            
+        }
+
     }
 }
