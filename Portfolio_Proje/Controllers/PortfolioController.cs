d2caf9e0fa522de1b23fc60aa0683977a694e06f
Hüseyin <hkoc274@gmail.com>
2024-12-24 01:45:47 +0300

    Fluent Validation paketi eklendi

--- Portfolio_Proje/Controllers/PortfolioController.cs
+++ Portfolio_Proje/Controllers/PortfolioController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntitiyFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Portfolio_Proje.Controllers
@@ -9,8 +10,27 @@ namespace Portfolio_Proje.Controllers
         PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolio());
         public IActionResult Index()
         {
+            ViewBag.v1 = "Proje listesi";
+            ViewBag.v2 = "Projeler ";
+            ViewBag.v3 = "Proje Listesi";
             var values=portfolioManager.TGetList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddPortfolio()
+        {
+            ViewBag.v1 = "Proje listesi";
+            ViewBag.v2 = "Projeler ";
+            ViewBag.v3 = "Proje Ekleme";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddPortfolio(Portfolio portfolio) 
+        {
+
+            portfolioManager.TAdd(portfolio);
+
+            return RedirectToAction("Index");
         }
     }
 }
