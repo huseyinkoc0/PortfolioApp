920ba2ed4c5d1f8573c7fb4c4633103257cb00aa
Hüseyin <hkoc274@gmail.com>
2024-12-25 02:21:47 +0300

    Feature kısmı düzenlendi

--- Portfolio_Proje/Controllers/FeatureController.cs
+++ Portfolio_Proje/Controllers/FeatureController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntitiyFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Portfolio_Proje.Controllers
 {
     public class FeatureController : Controller
     {
+        FeatureManager featureManager = new FeatureManager(new EfFeature());
         public IActionResult Index()
         {
-            return View();
+
+            ViewBag.v1 = "Düzenleme";
+            ViewBag.v2 = "Öne çıkarılanlar";
+            ViewBag.v3 = "Öne çıkarılanlar";
+            var values = featureManager.TGetById(1);
+            return View(values);
+        }
+         
+        
+        [HttpPost]
+        public IActionResult Index(Feature feature)
+        {
+
+            featureManager.TUpdate(feature);
+            return RedirectToAction("Index","Default");
         }
     }
 }
